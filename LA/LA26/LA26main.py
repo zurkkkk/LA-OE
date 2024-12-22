0d5c69d482f252d04dbfcddb4a443d7093443df4
zurkkkk <lloydabraham.delacruz@fcpc.edu.ph>
2024-12-23 01:22:06 +0800

    Create LA26main.py

---
+++ LA/LA26/LA26main.py
@@ -0,0 +1,11 @@
+import turtle as main
+
+Leonardo = main.Leonardo("Leonardo", "L")
+Michaelangelo = main.Michaelangelo("Michaelangelo", "M")
+Donatello = main.Donatello("Donatello", "D")
+Raphael = main.Raphael("Raphael", "R")
+
+print(Leonardo.alias)
+print(Michaelangelo.alias)
+print(Donatello.alias)
+print(Raphael.alias)
